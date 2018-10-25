baacdef53a6d4d3ff16a9bee28ba31b456b387ee
Charles Robertson and Sandra Castaneda <sandra@>
2018-10-25 11:27:36 -0700

    will not accept numbers greater than, or = to 4000

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function toRoman (num) {
   var result = '';
   var decimal = ['1000','900','500','400','100','90','50','40','10','9','5','4','1']
   var roman  = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
+  if (num >= 4000) {
+    return "Cannot Commpute"
+  }
   for (var i = 0; i <= decimal.length; i++){
     while (num%decimal[i] < num) {
       result += roman [i];
