d3ca3a39a7857d4b75bd923147ddea8f42890e12
Charles Robertson and Sandra Castaneda <sandra@>
2018-10-25 10:59:11 -0700

    add user logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+
 function toRoman (num) {
   var result = '';
-  var decimal = ['1000','900','500','400','100','90','50','40,','10',',9,','5','4','1']
+  var decimal = ['1000','900','500','400','100','90','50','40','10','9','5','4','1']
   var roman  = ['M','CM','D','CD','C','XC','L','XL','X','IX','V','IV','I'];
   for (var i = 0; i <= decimal.length; i++){
     while (num%decimal[i] < num) {
@@ -8,5 +9,20 @@ function toRoman (num) {
       num -= decimal [i];
     }
 }
-return result;
+    return result;
 }
+
+// User Logic
+
+$(document).ready(function() {
+  $("#numbers").submit(function(event) {
+    event.preventDefault();
+    var num = parseInt($("#numerals").val());
+    var finalResult = toRoman(num);
+
+      $("#result").text(finalResult);
+  });
+
+
+
+  });
